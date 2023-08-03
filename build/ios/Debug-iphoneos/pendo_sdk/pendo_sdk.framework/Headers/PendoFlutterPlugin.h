6a9441784272afb79a7a7045fecaa5b87072a277
millesm <millesm@gmail.com>
2023-08-03 12:38:48 +1000

    Delete build directory

--- build/ios/Debug-iphoneos/pendo_sdk/pendo_sdk.framework/Headers/PendoFlutterPlugin.h
+++
@@ -1,5 +0,0 @@
-#import <Flutter/Flutter.h>
-
-@interface PendoFlutterPlugin : NSObject<FlutterPlugin>
-+ (void)registerWithRegistry:(NSObject<FlutterPluginRegistry>*)registry NS_SWIFT_NAME(registerWithRegistry(_:));
-@end
